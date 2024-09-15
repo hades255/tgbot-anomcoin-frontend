6173c6fba06834a52c78199fa65428015e8ebbc6
hades255 <hades.255@outlook.com>
2024-09-15 14:58:17 -0400

    create coin&mouse move to eat

--- src/components/home/CoinGround.js
+++ src/components/home/CoinGround.js
@@ -4,14 +4,15 @@ import CoinItem from "./CoinItem";
 
 const CoinGround = () => {
   const [height, setHeight] = useState(window.innerHeight - 360);
-  const [width, setWidth] = useState(window.innerWidth - 24);
+  const [width, setWidth] = useState(window.innerWidth - 32);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
     const handleResize = () => {
       setHeight(window.innerHeight - 360);
-      setWidth(window.innerWidth - 24);
+      setWidth(window.innerWidth - 32);
     };
     window.addEventListener("resize", handleResize);
     return () => {
@@ -20,24 +21,40 @@ const CoinGround = () => {
   }, []);
 
   const createNewCoin = useCallback(() => {
-    if (coins.length >= 10) return;
-    setCoins([...coins, newCoin(width, height, 10)]);
+    if (coins.length >= 100) return;
+    setCoins([...coins, newCoin(width, height, 7)]);
   }, [height, width, coins]);
 
   useEffect(() => {
     const timer = setInterval(() => {
       createNewCoin();
-    }, 1000);
+    }, 500);
     return () => {
       clearInterval(timer);
     };
   }, [createNewCoin]);
 
+  const handleMouseMove = useCallback((event) => {
+    setMousePosition({
+      x: event.clientX - 16,
+      y: event.clientY - 196,
+    });
+  }, []);
+
+  useEffect(() => {
+    const remaining = coins.filter(
+      (item) =>
+        Math.abs(mousePosition.x - 42 - item.x) > 50 ||
+        Math.abs(mousePosition.y - 32 - item.y) > 40
+    );
+    if (remaining.length !== coins.length) setCoins(remaining);
+  }, [mousePosition, coins]);
+
   return (
-    <div className="mt-4 h-[calc(100vh_-_360px)] w-full bg-[#3333] relative">
-      <span className="text-white">
-        {width} {height}
-      </span>
+    <div
+      className="mt-4 h-[calc(100vh_-_360px)] w-full relative"
+      onMouseMove={handleMouseMove}
+    >
       {coins.map((item, index) => (
         <CoinItem key={index} coin={item} />
       ))}
--- src/components/home/CoinItem.js
+++ src/components/home/CoinItem.js
@@ -1,10 +1,24 @@
 import React from "react";
 import DefaultCoin from "../../assets/icons/coins/Default";
+import Coin1Icon from "../../assets/icons/coins/Coin1";
+import Coin2Icon from "../../assets/icons/coins/Coin2";
+import Coin7Icon from "../../assets/icons/coins/Coin7";
+import Coin3Icon from "../../assets/icons/coins/Coin3";
+import Coin4Icon from "../../assets/icons/coins/Coin4";
+import Coin5Icon from "../../assets/icons/coins/Coin5";
+import Coin6Icon from "../../assets/icons/coins/Coin6";
 
 const CoinItem = ({ coin }) => {
   return (
     <div style={{ position: "absolute", left: coin.x, top: coin.y }}>
-      <DefaultCoin />
+      {coin.type === 0 && <DefaultCoin />}
+      {coin.type === 1 && <Coin1Icon />}
+      {coin.type === 2 && <Coin2Icon />}
+      {coin.type === 3 && <Coin3Icon />}
+      {coin.type === 4 && <Coin4Icon />}
+      {coin.type === 5 && <Coin5Icon />}
+      {coin.type === 6 && <Coin6Icon />}
+      {coin.type === 7 && <Coin7Icon />}
     </div>
   );
 };
