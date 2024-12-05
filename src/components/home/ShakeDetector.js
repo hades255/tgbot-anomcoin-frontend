17795ae2be07855a3fd51b29474661c504804608
hades255 <hades.255@outlook.com>
2024-12-06 01:07:05 +0900

    shake-exp

---
+++ src/components/home/ShakeDetector.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+
+const ShakeDetector = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const shakingThreshold = 15;
+    let lastX = null;
+    let lastY = null;
+    let lastZ = null;
+
+    const handleMotion = (event) => {
+      const acceleration = event.accelerationIncludingGravity;
+      const currentX = acceleration.x;
+      const currentY = acceleration.y;
+      const currentZ = acceleration.z;
+
+      if (lastX !== null && lastY !== null && lastZ !== null) {
+        const deltaX = Math.abs(lastX - currentX);
+        const deltaY = Math.abs(lastY - currentY);
+        const deltaZ = Math.abs(lastZ - currentZ);
+
+        if (
+          deltaX > shakingThreshold ||
+          deltaY > shakingThreshold ||
+          deltaZ > shakingThreshold
+        ) {
+          console.log("Shake detected!");
+          setCount((prev) => prev + 1);
+          // You can handle the shake detection by invoking a function or updating state here
+        }
+      }
+
+      lastX = currentX;
+      lastY = currentY;
+      lastZ = currentZ;
+    };
+
+    if (window.DeviceMotionEvent) {
+      window.addEventListener("devicemotion", handleMotion);
+    }
+
+    return () => {
+      if (window.DeviceMotionEvent) {
+        window.removeEventListener("devicemotion", handleMotion);
+      }
+    };
+  }, []);
+
+  return <div className="fixed top-0 left-0 bg-white z-50">{count}</div>;
+};
+
+export default ShakeDetector;
